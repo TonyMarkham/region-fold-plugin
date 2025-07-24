caff258d600b1dd954312904f5b5d0118f15efad
TonyMarkham <49080394+TonyMarkham@users.noreply.github.com>
2025-07-24 18:14:28 -0400

    0.1.1

--- src/main/kotlin/com/github/tonymarkham/regionfoldplugin/folding/RegionFoldingBuilder.kt
+++ src/main/kotlin/com/github/tonymarkham/regionfoldplugin/folding/RegionFoldingBuilder.kt
@@ -3,6 +3,7 @@ package com.github.tonymarkham.regionfoldplugin.folding
 import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingBuilder
 import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
@@ -11,19 +12,33 @@ import com.intellij.psi.util.PsiTreeUtil
 
 class RegionFoldingBuilder : FoldingBuilder {
 
+    companion object {
+        private val LOG = Logger.getInstance(RegionFoldingBuilder::class.java)
+    }
+
     override fun buildFoldRegions(node: ASTNode, document: Document): Array<FoldingDescriptor> {
         val descriptors = mutableListOf<FoldingDescriptor>()
         val element = node.psi
+        
+        // Debug logging
+        LOG.info("RegionFoldingBuilder called for language: ${element.language.id}")
+        LOG.info("Element type: ${element.javaClass.simpleName}")
+        LOG.info("File extension: ${element.containingFile?.virtualFile?.extension}")
 
         // Find all region start comments
         val allComments = PsiTreeUtil.findChildrenOfType(element, PsiComment::class.java)
+        LOG.info("Found ${allComments.size} comments")
+        
         val regionStarts = mutableListOf<PsiComment>()
 
         for (comment in allComments) {
             val text = comment.text.trim()
+            LOG.info("Comment text: '$text'")
+            
             // Only match the exact pattern: starts with "/* #region" and ends with "*/"
             if (text.startsWith("/* #region") && text.endsWith("*/")) {
                 regionStarts.add(comment)
+                LOG.info("Found region start: $text")
             }
         }
 
@@ -37,10 +52,12 @@ class RegionFoldingBuilder : FoldingBuilder {
                 if (startOffset < endOffset) {
                     val textRange = TextRange(startOffset, endOffset)
                     descriptors.add(FoldingDescriptor(regionStart.node, textRange))
+                    LOG.info("Created folding descriptor from $startOffset to $endOffset")
                 }
             }
         }
 
+        LOG.info("Returning ${descriptors.size} folding descriptors")
         return descriptors.toTypedArray()
     }
 
